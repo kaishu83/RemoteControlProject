d5fca604fcfb9d9c7331cf0987c7609815cd9de6
kaishu83 <kaishu83@gmail.com>
2019-11-08 16:43:47 -0700

    progress

---
+++ src/RemoteControlApp.java
@@ -0,0 +1,13 @@
+
+public class RemoteControlApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+// TODO: Add pressButton() method
+// TODO: Add 
