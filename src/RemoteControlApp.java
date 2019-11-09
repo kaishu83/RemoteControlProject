c997304d649035a1de8b90d37e9fe5f9d0502315
kaishu83 <kaishu83@gmail.com>
2019-11-08 17:52:41 -0700

    Completed

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -9,10 +9,33 @@ public class RemoteControlApp {
 		while (!opt.equals("ON")) {
 			initialMessage();
 			opt = input.next();
-			
-		}
 
-		System.out.println("TURNING ON...");
+		}
+		turningOn();
+		pressButton();
+
+		do {
+			opt = input.next();
+			switch (opt) {
+				case "0":
+				case "1":
+				case "2":
+				case "3":
+				case "4":
+				case "5":
+				case "6":
+				case "7":
+				case "8":
+				case "9":
+					pressButton();
+					break;
+				default:
+					System.out.println("Command not recognized");
+					System.out.println("Please enter a number,enter \"OFF\" to get out");
+					break;
+			}
+		} while (!opt.equals("OFF"));
+		turningOff();
 	}
 
 	public static void initialMessage() {
@@ -22,6 +45,14 @@ public class RemoteControlApp {
 
 	public static void pressButton() {
 		System.out.println("BOOP");
+		System.out.println("Please enter a number,enter \"OFF\" to get out");
 	}
 
+	public static void turningOff() {
+		System.out.println("Turning off");
+	}
+
+	public static void turningOn() {
+		System.out.println("TURNING ON...");
+	}
 }
