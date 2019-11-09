1fe10d621aaea7185131e13d38fa1dfe884c55aa
kaishu83 <kaishu83@gmail.com>
2019-11-08 17:19:20 -0700

    system turned on

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -1,12 +1,27 @@
+import java.util.Scanner;
 
 public class RemoteControlApp {
 
 	public static void main(String[] args) {
-		
+		Scanner input = new Scanner(System.in);
+		initialMessage();
+		String opt = input.next();
+		while (!opt.equals("ON")) {
+			initialMessage();
+			opt = input.next();
+			
+		}
+
+		System.out.println("TURNING ON...");
+	}
+
+	public static void initialMessage() {
+		System.out.println("To begin, turn on the remote control(\"Enter ON\")");
+
+	}
+
+	public static void pressButton() {
+		System.out.println("BOOP");
 	}
 
 }
-
-
-// TODO: Add pressButton() method
-// TODO: Add 
